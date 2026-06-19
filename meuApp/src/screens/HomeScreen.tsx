0ed738654c7f7a07545c4e2a613d408d040a6239
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-19 14:36:39 -0400

    Estilizando a pagina Home parte 2

--- meuApp/src/screens/HomeScreen.tsx
+++ meuApp/src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen({ navigation }: any) {
   return (
@@ -13,18 +14,26 @@ export default function HomeScreen({ navigation }: any) {
       style={styles.background}
     >
       <View style={styles.overlay}>
-        <Text style={styles.titulo}>Mapa de Memórias</Text>
+        <View style={styles.topo}>
+          <Text style={styles.titulo}>Mapa de</Text>
 
-        <Text style={styles.subtitulo}>
-          Colecione os momentos que merecem ser lembrados.
-        </Text>
+          <Text style={styles.memorias}>Memórias</Text>
 
-        <TouchableOpacity
-          style={styles.botao}
-          onPress={() => navigation.navigate("MapaMemorias")}
-        >
-          <Text style={styles.textoBotao}>Começar Jornada</Text>
-        </TouchableOpacity>
+          <Text style={styles.subtitulo}>
+            Cada destino guarda uma história.
+          </Text>
+
+          <View style={styles.botoes_inferiores}>
+            <TouchableOpacity
+              style={styles.botao}
+              onPress={() => navigation.navigate("MapaMemorias")}
+            >
+              <Ionicons name="camera-outline" size={24} color="#FFFFFF" />
+              <Text style={styles.textoBotao}>Colecione seus momentos</Text>
+            </TouchableOpacity>
+            <Text style={styles.textoSecundario}>Começar jornada →</Text>
+          </View>
+        </View>
       </View>
     </ImageBackground>
   );
@@ -38,37 +47,67 @@ const styles = StyleSheet.create({
 
   overlay: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 30,
-    backgroundColor: "rgba(0,0,0,0.25)",
+    paddingTop: 120,
+    paddingBottom: 80,
+    backgroundColor: "rgba(0,0,0,0.18)",
+  },
+  topo: {
+    alignItems: "center",
   },
 
   titulo: {
-    fontSize: 42,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    marginBottom: 15,
+    fontSize: 52,
+    color: "#4D2F1F",
+    fontStyle: "italic",
+    fontWeight: "300",
+  },
+
+  memorias: {
+    fontSize: 68,
+    color: "#6B8CB0",
+    fontStyle: "italic",
+    fontWeight: "300",
+    marginTop: -10,
   },
 
   subtitulo: {
+    marginTop: 20,
     fontSize: 18,
+    marginBottom: 60,
     textAlign: "center",
-    color: "#FFFFFF",
-    marginBottom: 50,
-    lineHeight: 28,
+    color: "white",
+    lineHeight: 32,
+  },
+  botoes_inferiores: {
+    alignItems: "center",
   },
 
   botao: {
-    backgroundColor: "#79A3C3",
-    paddingVertical: 16,
-    paddingHorizontal: 45,
-    borderRadius: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#6B8CB0",
+    width: "80%",
+    paddingRight: 10,
+    paddingVertical: 20,
+    borderRadius: 40,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 5,
   },
 
   textoBotao: {
     color: "#FFFFFF",
     fontSize: 18,
     fontWeight: "600",
+    marginLeft: 10,
+  },
+  textoSecundario: {
+    marginTop: 20,
+    color: "#FFFFFF",
+    fontSize: 18,
   },
 });
