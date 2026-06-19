22dec90f8c3b91baeca2ba7a08f7904a55f97b80
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-19 11:15:27 -0400

    Navegação configurada com sucesso da tela Home e Mapa das memórias
    Instalação do recurso para navegação em abas:
    - npx expo install @react-navigation/bottom-tabs  //navegação por aba

--- meuApp/App.tsx
+++ meuApp/App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { HomeScreen } from "./src/screens/HomeScreen";
+import { MapaMemorias } from "./src/screens/MapaMemorias";
+
+export type RootTabParamList = {
+  Home: undefined;
+  MapaMemorias: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootTabParamList>(); //serve para a navegação por aba
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="MapaMemorias" component={MapaMemorias} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
