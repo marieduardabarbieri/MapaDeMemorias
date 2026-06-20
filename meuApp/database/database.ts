dc67bb39b12e642341178da7d3bd8e4619071624
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-20 16:19:14 -0400

    Criação do database/database.ts

---
+++ meuApp/database/database.ts
@@ -0,0 +1,13 @@
+import { SQLiteDatabase } from "expo-sqlite";
+
+export async function inicializarBanco(db: SQLiteDatabase) {
+  await db.execAsync(`
+    CREATE TABLE IF NOT EXISTS memorias (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      pais TEXT NOT NULL,
+      cidade TEXT NOT NULL,
+      data TEXT NOT NULL,
+      descricao TEXT NOT NULL
+    );
+  `);
+}
