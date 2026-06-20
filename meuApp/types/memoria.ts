8f31d61c8495ab8d2ae55627fcb66d607dcd0d1a
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-06-20 16:17:42 -0400

    Criação types/memoria.ts

---
+++ meuApp/types/memoria.ts
@@ -0,0 +1,14 @@
+export type Memoria = {
+  id: number;
+  pais: string;
+  cidade: string;
+  data: string;
+  descricao: string;
+};
+
+export type NovaMemoria = {
+  pais: string;
+  cidade: string;
+  data: string;
+  descricao: string;
+};
